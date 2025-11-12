e0bd5fbb13d2eb1a0eefc6016594600d1c6d9e84
Екатерина <pygavko@yandex.ru>
2025-11-12 17:25:49 +0300

    Подготовить сборку проекта Where To Go With Friends.

---
+++ main-service/src/main/java/com/my/pet/project/where_to_go_with_friends/MainServiceWhereToGo.java
@@ -0,0 +1,11 @@
+package com.my.pet.project.where_to_go_with_friends;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MainServiceWhereToGo {
+    public static void main(String[] args) {
+        SpringApplication.run(MainServiceWhereToGo.class, args);
+    }
+}
---
+++ stat-service/stat-server/src/main/java/com/my/pet/project/StatServerService.java
@@ -0,0 +1,11 @@
+package com.my.pet.project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StatServerService {
+    public static void main(String[] args) {
+        SpringApplication.run(StatServerService.class, args);
+    }
+}
